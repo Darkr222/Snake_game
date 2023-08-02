d6ec88ea22751ee8332aa5ae1b63938b87a92956
Darkr222 <72606048+Darkr222@users.noreply.github.com>
2023-08-02 17:05:30 +0530

    snake foods eating

--- Assets/scripts/Food.cs
+++ Assets/scripts/Food.cs
@@ -30,4 +30,12 @@ public class Food : MonoBehaviour
 
 
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _randomizedPosition();
+        }
+    }
 }
